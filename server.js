e396ee53f981ab843adb7dc296435cc542523f98
Weggie Villarante <jluigi.phcci@gmail.com>
2022-12-11 22:13:31 +0800

    adding socket io.

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const apiRouter = require('./routes/orderRoutes');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+const apiRouter = require('./routes/orderRoutes');
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.emit('message', 'Welcome to labada');
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'user disconnected.');
+        console.log('user disconnected');
+    });
+  });
 
 app.use(express.json());
-
 app.use('/api', apiRouter);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening at port ${port}...`));
+server.listen(port, () => console.log(`Listening at port ${port}...`));
